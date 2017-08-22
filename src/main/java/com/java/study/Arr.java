bcabd11ef8a282344f84eea32c8d03a90467003e
aloyschen <813971830@qq.com>
2017-08-22 11:01:16 +0800

    2017.08.22 AM 11:01

--- src/main/java/com/java/study/Arr.java
+++ src/main/java/com/java/study/Arr.java
@@ -9,17 +9,34 @@ public class Arr {
     */
         System.out.println(Arrays.toString(arr));
     }
+    /*
+    *调用Arrays.copyOf可以在原有基础上扩展容量
+     */
     public static void Arr_copy(int[] arr, int[] arr_copy){
 
-        arr = Arrays.copyOf(arr, arr.length);
+        arr = Arrays.copyOf(arr, 20);
+        System.out.println(Arrays.toString(arr));
+    }
+    /*
+    * 调用System.copy实现两个数组对象的浅拷贝，不会新建一个对象，底层为c/c++
+     */
+    public static int[] Arr_copy(int[] arr){
+        int[] copy = new int[arr.length];
+        System.arraycopy(arr,0, copy,0, arr.length);
+        System.out.println(Arrays.toString(copy));
+        return copy;
     }
     public static void main(String args[]){
         int[] arr = new int[11];
         int[] arr_copy = new int[11];
+        int[] copy = new int[11];
         for(int i = 0; i <= 10; i++)
             arr[i] = i;
         Arr_print(arr);
         Arr_copy(arr, arr_copy);
+        copy = Arr_copy(arr);
+
+
     }
 
 }
